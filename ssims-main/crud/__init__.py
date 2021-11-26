58e61ea3a807271ca1fd666b1a46857b0debd8bc
unknown <angeliquejoy.canete@g.msuiit.edu.ph>
2021-11-26 12:23:07 +0800

    flask update

---
+++ ssims-main/crud/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flaskext.mysql import MySQL
+
+application = Flask(__name__)
+
+my_sql = MySQL()
+application.config["SECRET_KEY"] = "james"
+application.config["SECRET_KEY"] = "secret_key"
+application.config["MYSQL_DATABASE_HOST"] = "localhost"
+application.config["MYSQL_DATABASE_USER"] = "root"
+application.config["MYSQL_DATABASE_PASSWORD"] = "root0305"
+application.config["MYSQL_DATABASE_DB"] = "flask"
+
+my_sql.init_app(application)
+
+from crud import routes
