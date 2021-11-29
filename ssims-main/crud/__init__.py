f3e693e42147594a848f9796816543d0a0138b8f
ajcnt <89629346+hyejiea@users.noreply.github.com>
2021-11-29 12:02:03 +0800

    Update __init__.py

--- ssims-main/crud/__init__.py
+++ ssims-main/crud/__init__.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 application = Flask(__name__)
 
 my_sql = MySQL()
-application.config["SECRET_KEY"] = "james"
+application.config["SECRET_KEY"] = "weh"
 application.config["SECRET_KEY"] = "secret_key"
 application.config["MYSQL_DATABASE_HOST"] = "localhost"
 application.config["MYSQL_DATABASE_USER"] = "root"
